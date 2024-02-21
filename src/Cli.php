81d842091b02b229c81d362b5ec413eb00ffe33a
motoart31@gmail.com <Oleg Rabinovich>
2024-02-21 03:25:03 +0300

    fixed autoload composer, enclude function brain and cli

--- src/Cli.php
+++ src/Cli.php
@@ -1,8 +1,13 @@
 <?php
-
 namespace Php\Project\Cli;
+
+use function cli\line;
+use function cli\prompt;
 
 function Hello()
 {
-print_r('Hello!!!!!!!!!');
+line('Welcome to the Brain Game!');
+$name = prompt('May I have your name?');
+line("Hello, %s!", $name);
+
 }
